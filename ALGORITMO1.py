e9af36f786719cae35e821a270a92fec59dfd7fa
JosEdu25 <143762972+JosEdu25@users.noreply.github.com>
2023-12-01 21:34:09 -0500

    Update ALGORITMO1.py

--- ALGORITMO1.py
+++ ALGORITMO1.py
@@ -1,2 +1,48 @@
 #Bubble sort (ordenamiento burbuja)
-DE JOSE
+def Datos():
+    # Función para ingresar datos a una lista
+    lista = []
+    
+    while True:
+        n = int(input("Ingresa los datos. Usa 0 para terminar: "))
+        if n == 0:
+            return lista  # Retorna la lista cuando se ingresa 0
+        else:
+            lista.append(n)  # Agrega el número a la lista si no es 0
+    return lista
+
+def burbuja(lista):
+    # Implementación del método de ordenamiento burbuja
+    tamano = len(lista)
+    
+    # Bucle externo: recorre la lista para cada pase
+    for _ in range(0, tamano):
+        
+        # Bucle interno: compara y ordena los elementos adyacentes
+        for j in range(0, tamano-1):
+            if lista[j] > lista[j+1]:
+                # Intercambia elementos si están en el orden incorrecto
+                aux = lista[j]
+                lista[j] = lista[j+1]
+                lista[j+1] = aux
+                
+    return lista
+
+def mostrarLista(lista):
+    # Muestra la lista en orden ascendente y descendente
+    tam = len(lista)
+    
+    print("Orden de manera ascendente:")
+    for i in range(0, tam):
+        print(f"{lista[i]}")
+    
+    print("Orden de manera descendente:")
+    
+    # Bucle inverso: muestra la lista en orden descendente
+    for i in range(tam, 0, -1):
+        print(f"{lista[i-1]}")
+
+# Ejecución del programa
+lista = Datos()
+lista = burbuja(lista)
+mostrarLista(lista)
